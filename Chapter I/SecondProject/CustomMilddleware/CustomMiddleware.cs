6773bd84d7ab0f32a7c523566b8a56b79b9083e1
Ariyan-2007 <ariyanjahangireng@gmail.com>
2024-09-15 16:21:38 +0600

    Second Day

--- Chapter I/SecondProject/CustomMilddleware/CustomMiddleware.cs
+++ Chapter I/SecondProject/CustomMilddleware/CustomMiddleware.cs
@@ -5,9 +5,18 @@ namespace MiddlewareExample.CustomMiddlware
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await context.Response.WriteAsync("My Middleware - Starting");
+            await context.Response.WriteAsync("My Middleware - Starting\n");
             await next(context);
-            await context.Response.WriteAsync("My Middleware - Ending");
+            await context.Response.WriteAsync("My Middleware - Ending\n");
+        }
+    }
+
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder app)
+        {
+
+            return app.UseMiddleware<CustomMiddleware>();
         }
     }
 }
--- Chapter I/SecondProject/Program.cs
+++ Chapter I/SecondProject/Program.cs
@@ -7,19 +7,21 @@ var app = builder.Build();
 // Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello ");
+    await context.Response.WriteAsync("Hello \n");
 
     await next(context);
 });
 
 //Middleware 2
 
-app.UseMiddleware<CustomMiddleware>();
+// app.UseMiddleware<CustomMiddleware>();
+// app.UseCustomMiddleware();
+app.UseNamingMiddleware();
 
 //Middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("World");
+    await context.Response.WriteAsync("World \n");
 });
 
 app.Run();
